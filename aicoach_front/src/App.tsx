fb192ba7fe7f5ea1441fd635f56279e78883cbe4
kala_allan <kalaitawa@gmail.com>
2026-04-08 15:04:08 +0200

    first commit

---
+++ aicoach_front/src/App.tsx
@@ -0,0 +1,13 @@
+import { } from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white p-10">
+      Tailwind OK 🚀
+    </div>
+  );
+}
+
+
+export default App
